2f44ab111a5726932c46112b72edfb17c775b237
Ryoya <111482921+Ryoya15@users.noreply.github.com>
2026-03-12 20:40:33 +0900

    Add files via upload

---
+++ ibus-monitor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eo pipefail
+test -x /usr/bin/ibus
+journalctl /usr/bin/gnome-shell -qfo cat | while read -r line; do
+    if [[ "$line" == *"Gio.DBusError: GDBus.Error:org.freedesktop.DBus.Error.Failed: Set global engine failed"* ]]; then
+        echo "IBus semi-crash detected. Restarting..."
+        ibus restart
+    fi
+done
