f8bb0e4a83393f5bf3a53ddbd553baa8e670e4d0
Ryoya <111482921+Ryoya15@users.noreply.github.com>
2026-03-17 17:24:41 +0900

    Add sleep after IBus restart to prevent rapid restarts

--- ibus-monitor.sh
+++ ibus-monitor.sh
@@ -5,5 +5,6 @@ journalctl /usr/bin/gnome-shell -qfo cat | while read -r line; do
     if [[ "$line" == *"Gio.DBusError: GDBus.Error:org.freedesktop.DBus.Error.Failed: Set global engine failed"* ]]; then
         echo "IBus semi-crash detected. Restarting..."
         ibus restart
+        sleep 1
     fi
 done
