88db64248b314e5c7cf08e227a4c7145d5dcd17f
Ryoya <111482921+Ryoya15@users.noreply.github.com>
2026-03-17 22:58:44 +0900

    追加フィアルのダウンロード方法を改良、nowエラー抑制など

--- install.sh
+++ install.sh
@@ -15,22 +15,36 @@ else
 fi
 
 if [ "$1" = "uninstall" ]; then
-    systemctl --$unitscope disable --now $unitname
+    systemctl --$unitscope disable --now $unitname 2>/dev/null
 
     rm -f $unitpath$unitname $scriptpath$scriptname
 
-    echo "$appname has been uninstalled. ($unitscope mode)"
+    echo "$appname has been uninstalled ($unitscope)."
 else
     mkdir -p $scriptpath $unitpath
+
+    download(){
+        # $1=source(filename only)
+        # $2=destination(fullpath)
+        urlprefix=https://github.com/Ryoya15/ibus-monitor/raw/refs/heads/main/
+        localprefix=$(dirname "$0")/
+        wget -q "$urlprefix$1" -O "$2" 2>/dev/null || \
+        curl -fsL "$urlprefix$1" -o "$2" 2>/dev/null || \
+        cp "$localprefix$1" "$2" 2>/dev/null || {
+            echo "Error: Unable to retrieve the additional files. If you are not connected to the Internet or cannot use either wget or curl, please run install.sh from the downloaded source code."
+            exit 1
+        }
+    }
     
-    wget -qO $scriptpath$scriptname https://github.com/Ryoya15/ibus-monitor/raw/refs/heads/main/$scriptname
+    download $scriptname $scriptpath$scriptname
     chmod +x $scriptpath$scriptname
 
-    wget -qO $unitpath$unitname https://github.com/Ryoya15/ibus-monitor/raw/refs/heads/main/$unitname
+    download $unitname $unitpath$unitname
     [ "$unitscope" = "global" ] && sed -i "s|%h/.local/bin/|$scriptpath|" $unitpath$unitname
 
-    systemctl --$unitscope enable --now $unitname
-    echo "$appname has been installed. ($unitscope mode)"
+    systemctl --$unitscope enable --now $unitname 2>/dev/null
+    
+    echo "$appname has been installed ($unitscope)."
 fi
 
-[ "$unitscope" = "global" ] && echo "Global mode cannot operate the current user's unit. Please restart the computer to complete the task."
+[ "$unitscope" = "global" ] && echo "A restart is required for this change to take full effect."
